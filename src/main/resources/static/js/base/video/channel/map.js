bc1434dd97de9621dfdd974273ec1864a18e27b5
AB <ab@chromium.org>
2019-07-12 20:22:30 +0800

    图标

--- src/main/resources/static/js/base/video/channel/map.js
+++ src/main/resources/static/js/base/video/channel/map.js
@@ -37,7 +37,7 @@ function markerClicked(channelId) {
 
 
 function showDevices(map, devices) {
-    var cameraIcon = new BMap.Icon("/images/2.png", new BMap.Size(30, 30));
+    var cameraIcon = new BMap.Icon("/images/2.png", new BMap.Size(35, 35));
     var zoomLevel = localStorage.getItem('zoomLevel');
 
     for (var i = 0; i < devices.length; i++) {
