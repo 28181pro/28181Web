6e6cce9838fd25028b90fd5958e2fc482bfd25b8
AkonYan <Nooootice@outlook.com>
2019-08-20 15:26:34 +0800

    播放器相关脚本

---
+++ src/main/resources/static/js/base/video/channel/video-bk.js
@@ -0,0 +1,36 @@
+/**
+ * 播放js
+ */
+var vm = new Vue({
+    el:'#dpLTE',
+    data: {
+        channel: {
+            id: location.search.split('id=')[1]
+        }
+    },
+    methods : {
+        setForm: function() {
+            $.SetForm({
+                url: '../../video/channel/getChannelInfo?_' + $.now(),
+                param: vm.channel.id,
+                success: function(data) {
+                    vm.channel = data;
+                    alert( data.channelCode )
+                }
+            });
+        }
+    },
+    mounted: function () {
+        var channelId = this.channel.id;
+        console.log("Channel id=", channelId);
+        if (channelId) {
+            $.SetForm({
+                url: 'getChannelInfo?_' + $.now(),
+                param: channelId,
+                success: function(data) {
+                    vm.channel = data;
+                }
+            });
+        }
+    }
+})
