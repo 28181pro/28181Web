4fa79aec23a35126a5eb4696a9fd7baa7abd10df
yulibin <yulibin05010332@163.com>
2020-09-02 09:31:43 +0800

    提交图片上传与回显Demo代码，在首页

--- src/main/java/com/cethik/irmp/modules/base/HomeController.java
+++ src/main/java/com/cethik/irmp/modules/base/HomeController.java
@@ -7,9 +7,14 @@ import com.cethik.irmp.modules.sys.controller.AbstractController;
 import com.cethik.irmp.modules.video.service.ChannelService;
 import com.cethik.irmp.modules.video.service.GbDeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -81,5 +86,36 @@ public class HomeController extends AbstractController {
             return CommonUtils.msg("系统异常");
         }
     }
+
+
+    @RequestMapping("/uploadsave")
+    public R upload(@RequestParam("file") MultipartFile file, Model model) {
+        try {
+//            // 获取上传文件名
+            String filename = file.getOriginalFilename();
+            String filePath = "/opt/images";
+            String path = filePath;
+            // 新建文件
+            File filepath = new File(path, filename);
+            // 判断路径是否存在，如果不存在就创建一个
+            if (!filepath.getParentFile().exists()) {
+                filepath.getParentFile().mkdirs();
+            }
+            try {
+                // 写入文件
+                file.transferTo(new File(path + File.separator + filename));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 将src路径发送至html页面
+            String sFileName = "/images" + File.separator + filename;
+            return R.ok().put("filename", sFileName);
+
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+            return null;
+        }
+    }
+
 
 }
